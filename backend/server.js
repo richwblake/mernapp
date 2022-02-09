253b9157a739699349c3816cdfd338dd2035c7f4
wills blake <richwblake@gmail.com>
2022-02-08 16:03:06 -0800

    Connected server to Mongo DB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
+const connectDB = require('./config/db');
 const port = process.env.PORT || 3000;
+
+connectDB();
 
 const app = express();
 
