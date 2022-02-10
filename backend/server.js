623019ae446820b6c58fbd1c6ea19bc9de61576c
wills blake <richwblake@gmail.com>
2022-02-09 21:52:09 -0800

    added frontend folder

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
+const cors = require('cors');
 const connectDB = require('./config/db');
 const port = process.env.PORT || 3000;
 
 connectDB();
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
